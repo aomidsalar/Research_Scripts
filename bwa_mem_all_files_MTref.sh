d406af907e60f0dad5eb7e25cbbfe9e2436b8298
aomidsalar <62361776+aomidsalar@users.noreply.github.com>
2021-07-17 11:13:57 -0700

    Update bwa_mem_all_files_MTref.sh

--- bwa_mem_all_files_MTref.sh
+++ bwa_mem_all_files_MTref.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for i in $(ls -1 *R1.fastq | sed 's/\.R1.fastq//')
 do
-bwa mem -t 16 /home/aomidsal/mitoreference_MT/MT.fa $i\.R1.fastq $i\.R2.fastq | samtools view -bS - | samtools sort -@ 16 -o $i\.MTref.sorted.bam - ; done
+bwa mem -t 16 <path_to>/MT.fa $i\.R1.fastq $i\.R2.fastq | samtools view -bS - | samtools sort -@ 16 -o $i\.MTref.sorted.bam - ; done
